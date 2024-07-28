64203d32c312a1929b58769d6d451d5787264c01
zjx-kimi <zhongjiaxuankimi@qq.com>
2024-07-28 17:22:18 +0800

    Create SP folders and 莫队's some code and pictures
    创建SP文件夹和一些莫队的代码和图片

---
+++ AT/ABC242G.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int N = 1e6 + 5;
+int n, m;
+int a[N], pos[N], cnt[N];
+int ans[N];
+int now;
+struct Q {
+    int l, r, k;
+} q[N];
+int calc(int x) { return x * (x - 1) / 2; }
+void Add(int n) {
+    cnt[a[n]]++;
+    if (cnt[a[n]] % 2 == 0) now++;
+}
+void Sub(int n) {
+    if (cnt[a[n]] % 2 == 0) now--;
+    cnt[a[n]]--;
+}
+bool cmp (Q x, Q y) { return pos[x.l] == pos[y.l] ? x.r < y.r : pos[x.l] < pos[y.l]; }
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    int siz = sqrt(n);
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i]; 
+        pos[i] = i / siz;
+    }
+    cin >> m;
+    for (int i = 0; i < m; ++i) {
+        cin >> q[i].l >> q[i].r;
+        q[i].k = i;
+    }
+    sort(q, q + m, cmp);
+    int l = 1, r = 0;
+    for (int i = 0; i < m; ++i) {
+        while (q[i].l < l) Add(--l);
+        while (q[i].r > r) Add(++r);
+        while (q[i].l > l) Sub(l++);
+        while (q[i].r < r) Sub(r--);
+        ans[q[i].k] = now;
+    }
+    for (int i = 0; i < m; ++i) cout << ans[i] << '\n';
+    return 0;
+} 
